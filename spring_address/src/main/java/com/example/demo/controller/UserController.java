ebf042f6732094451213c5053ed6f3cdb9dbfc9c
さつき <@192.168.2.107>
2020-09-04 17:14:34 +0900

    画面遷移

--- spring_address/src/main/java/com/example/demo/controller/UserController.java
+++ spring_address/src/main/java/com/example/demo/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,36 +25,8 @@ public class UserController {
    */
   @Autowired
   UserService userService;
-  /**
-   * ユーザー情報一覧画面を表示
-   * @param model Model
-   * @return ユーザー情報一覧画面
-   */
-  @GetMapping(value = "/user/list")
-  public String displayList(Model model) {
-    List<User> userlist = userService.searchAll();
-    model.addAttribute("userlist", userlist);
-    return "user/list";
-  }
-  /**
-   * ユーザー新規登録画面を表示
-   * @param model Model
-   * @return ユーザー情報一覧画面
-   */
-  @GetMapping(value = "/user/add")
-  public String displayAdd(Model model) {
-    model.addAttribute("userRequest", new UserRequest());
-    return "user/add";
-  }
-  /**
-   * ユーザー新規登録
-   * @param userRequest リクエストデータ
-   * @param model Model
-   */
 
-
-
-  @RequestMapping(value = "user/addCheck", method = RequestMethod.POST)
+  @RequestMapping(value = "user/AddCheck", method = RequestMethod.POST)
   public String create(@Validated @ModelAttribute UserRequest userRequest, BindingResult result, Model model) {
     if (result.hasErrors()) {
       List<String> errorList = new ArrayList<String>();
@@ -63,10 +34,36 @@ public class UserController {
         errorList.add(error.getDefaultMessage());
       }
       model.addAttribute("validationError", errorList);
-      return "user/add";
+      return "user/AddCheck";
     }
     // ユーザー情報の登録
     userService.create(userRequest);
     return "redirect:/user/list";
   }
+
+
+  @RequestMapping(value = "user/Edit", method = RequestMethod.GET)
+	public String Edit(Model model){
+              // 空のフォームオブジェクトをModelに設定
+		model.addAttribute("User", new User());
+              // 遷移先を返す
+		return "user/Edit";
+	}
+
+  @RequestMapping(value = "user/EditCheck", method = RequestMethod.POST)
+	public String EditCheck(@ModelAttribute("User") User form) {
+              // 遷移先を返す
+		return "user/EditCheck";
+	}
+
+
+@RequestMapping(value = "user/ｌist", method = RequestMethod.POST)
+public String Delete(Model model){
+          // 空のフォームオブジェクトをModelに設定
+	model.addAttribute("User", new User());
+          // 遷移先を返す
+	return "user/Delete";
+}
+
+
 }
